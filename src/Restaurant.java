97ba2d000ff2ce2a3f46356e6b1bc85a7073d764
Esther Kung <estherkung.c@gmail.com>
2021-07-19 19:03:01 -0700

    guess we were supposed to expect group orders..

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Restaurant {
     String name;
@@ -16,18 +17,31 @@ public class Restaurant {
         System.out.println("Medium is 1.25 times the price, Large is 1.5 times the price listed.");
 
         Scanner input = new Scanner(System.in);
+        boolean complete = false;
+        ArrayList<Pizza> order = new ArrayList<>();
+        ArrayList<Integer> amount = new ArrayList<>();
+        while (!complete) {
+            System.out.println("what type of pizza would you like to order?");
+            String type = input.next().toLowerCase();
 
-        System.out.println("what type of pizza would you like to order?");
-        String type = input.nextLine().toLowerCase();
+            System.out.println("what size would you like your " + type + " pizza?");
+            char size = input.next().toLowerCase().charAt(0);
 
-        System.out.println("how many " + type + " pizzas would you like to order?");
-        int amount = input.nextInt();
+            System.out.println("how many " + type + " pizzas of the indicated size would you like to order?");
+            amount.add(input.nextInt());
 
-        System.out.println("what size would you like your " + type + " pizza?");
-        char size = input.next().toLowerCase().charAt(0);
+            order.add(new Pizza(type, size));
 
-        Pizza order = new Pizza(type, size);
-        System.out.println("Your total is: $" + order.price(amount));
+            System.out.println("would you like to add more to your order?");
+            char cont = input.next().toLowerCase().charAt(0);
+
+            if (cont == 'n'){ complete = true; }
+        }
+        double total = 0.0d;
+        for (int i = 0; i < order.size(); i++){
+            total += order.get(i).price(amount.get(i));
+        }
+       System.out.println("Your total is: $" + total);
 
     }
 }
